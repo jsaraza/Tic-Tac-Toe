fd131b046ea67b7d9418391399dd371264c0d145
Jaime Saraza <70734810+jsaraza@users.noreply.github.com>
2023-09-24 16:36:20 -0700

    update change

--- TIc_Tac_Toe.py
+++ TIc_Tac_Toe.py
@@ -3,5 +3,49 @@ import pygame
 class tic_tac_toe:
     
     def __init__(self):
-        self.board = [['' for _ in range(3)] for _ in range(3)]
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.player = "X"
+
+    def click(self, x : int, y :int):
+        self.board[x][y] = self.player
+        checkWin()
+        if self.player == "O":
+            self.player = "X"
+        else:
+            self.player = "O"
+        
+
+    
+    def checkRow(self, row:int):
+        counter = 0
+        for col in range(3):
+            if self.board[row][col] == self.player:
+                counter += 1
+            else:
+                counter = 0
+            if counter == 3:
+                return True
+
+        return False
+    
+    def checkHorizontal(self, col: int):
+        counter = 0
+        for row in range(3):
+            if self.board[row][col] == self.player:
+                counter+=1
+            else:
+                counter = 0
+            if counter == 3:
+                return True
+        return False
+
+    def checkDiagonal(self):
+        x = 0
+        if self.board[x][x] == self.board[x + 1][x + 1] and self.board[x+2][x+2] == self.board[x + 1][x +1]:
+            return True
+        if self.board[x+2][x] == self.board[x + 1][x + 1] and self.board[x][x+2] == self.board[x + 1][x + 1]:
+            return True
+
+
+    def checkWin(self):
         
